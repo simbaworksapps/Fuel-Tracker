968585cc32cddd04a00293597675d9a4b033e5f8
Simba Works Apps <simbaworksapps@gmail.com>
2026-07-19 08:45:48 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-﻿const CACHE_NAME = "simba-fuel-tracker-v0.1.232";
+﻿const CACHE_NAME = "simba-fuel-tracker-v0.1.234";
 const ASSETS = [
   "/",
   "/index.html",
-  "/styles.css?v=0.1.232",
-  "/app.js?v=0.1.232",
+  "/styles.css?v=0.1.234",
+  "/app.js?v=0.1.234",
   "/manifest.json",
   "/assets/simba.jpg",
   "/icons/icon-192.png",
